21d4ccd7711d6e404295271bfa3d17bb961b6bae
mRoxBR <mateuspereira@visaojr.com.br>
2019-05-18 17:12:45 -0300

    Refatora "saveAsPNG( )"

--- src/main/fsm.js
+++ src/main/fsm.js
@@ -385,7 +385,7 @@ function saveAsPNG() {
 	var pngData = canvas.toDataURL('image/png');
 	
 	var win = window.open();
-	win.document.write('<iframe src="' + pngData + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+	win.document.write('<iframe src="' + pngData + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:800; height:600; background-color: #F9F9F9; border: 1px solid #999" allowfullscreen></iframe>');
 
 }
 
